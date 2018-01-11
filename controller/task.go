2193b5fa069426713af991c51cf3cddbeb3b92e1
Vincent Letourneau <vincent@nanoninja.com>
2018-01-11 15:07:49 +0100

    Implement Task controller

--- controller/task.go
+++ controller/task.go
@@ -110,13 +110,49 @@ func (t *Task) Create(w http.ResponseWriter, r *http.Request) {
 
 // FindById find a task by its ID.
 func (t *Task) FindById(w http.ResponseWriter, r *http.Request) {
+	id := handler.GetParams("id", r)
 
+	task, err := t.store.Find(id)
+	if err != nil {
+		log.Println(err)
+		handler.SendJSONError(w, "Not Found", http.StatusNotFound)
+		return
+	}
+
+	handler.SendJSONOk(w, task)
 }
 
+// Update updates a task.
 func (t *Task) Update(w http.ResponseWriter, r *http.Request) {
+	task := &model.Task{}
 
+	err := handler.ParseJSON(r, task)
+	if err != nil {
+		log.Println(err)
+		handler.SendJSONError(w, "Error while decoding to update", http.StatusBadRequest)
+		return
+	}
+
+	err = t.store.Update(task)
+	if err != nil {
+		log.Println(err)
+		handler.SendJSONError(w, "Error while updating task", http.StatusInternalServerError)
+		return
+	}
+
+	handler.SendJSONWithStatus(w, nil, http.StatusNoContent)
 }
 
+// Delete deletes a task by its ID.
 func (t *Task) Delete(w http.ResponseWriter, r *http.Request) {
+	id := handler.GetParams("id", r)
 
+	err := t.store.Delete(id)
+	if err != nil {
+		log.Println(err)
+		handler.SendJSONError(w, "Error while deleting task", http.StatusInternalServerError)
+		return
+	}
+
+	handler.SendJSONWithStatus(w, nil, http.StatusNoContent)
 }
